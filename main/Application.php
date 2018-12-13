645b20e22d7edb714312fbf6443b07c6e65ad6e6
chenshenchao <chenshenchao@outlook.com>
2018-12-13 20:46:26 +0800

    amend Application.

--- main/Application.php
+++ main/Application.php
@@ -1,20 +1,36 @@
 <?php namespace purplex;
 
+use Dotenv\Dotenv;
+use palex\Dictionary;
+use palex\Filesystem;
+
 /**
  * 
  */
 final class Application {
+    private $env;
+    private $setting;
+    private $filesystem;
+
     /**
      * 
      */
-    public function __construct($root) {
-        
+    public function __construct($projectPath) {
+        $this->env = new Dotenv($projectPath);
+        $this->setting = new Dictionary();
+        $this->filesystem = new Filesystem([
+            '@project' => $projectPath,
+            '@public' => $projectPath.Filesysem::DS.'public',
+            '@runtime' => $projectPath.Filesystem::DS.'runtime',
+        ]);
     }
 
     /**
      * 
      */
-    public function apply($start) {
-
+    public function apply($startTime) {
+        $this->env->load();
+        $debug = $_ENV['PURPLEX_DEBUG'] ?? false;
+        $trace = $_ENV['PURPLEX_TRACE'] ?? false;
     }
 }
